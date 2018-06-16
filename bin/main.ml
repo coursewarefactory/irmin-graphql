fab6f9741fa14587f4fbf68d42f44321c12d7f6d
Zach Shipko <zachshipko@gmail.com>
2018-06-15 19:23:23 -0700

    WIP Starting new API implementation

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,7 @@
 open Lwt.Infix
 
-module Store = Irmin_unix.Git.FS.Make
+module Store = Irmin_unix.Git.Make
+  (Irmin_unix.Git.G)
   (Irmin.Contents.String)
   (Irmin.Path.String_list)
   (Irmin_git.Branch(Irmin.Branch.String))
@@ -8,11 +9,11 @@ module Store = Irmin_unix.Git.FS.Make
 module Store_graphql = Irmin_graphql.Make(Store)
 
 let config =
-  let head = Git.Reference.of_raw "refs/heads/master" in
+  let head = Git.Reference.of_string "refs/heads/master" in
   Irmin_git.config ~head (Sys.argv.(1))
 
 let () =
-  let p = 
+  let p =
     Store.Repo.v config >>= fun repo ->
     Store.master repo >>= fun store ->
     Store_graphql.start_server store
