4b65937fc0874fe290189a314eef2d3172e70689
Zach Shipko <zachshipko@gmail.com>
2018-07-04 12:47:39 -0700

    Cleanup command line interface

--- bin/main.ml
+++ bin/main.ml
@@ -1,21 +1,36 @@
 open Lwt.Infix
+open Cmdliner
 
-module Store = Irmin_unix.Git.Make
-  (Irmin_unix.Git.G)
-  (Irmin.Contents.String)
-  (Irmin.Path.String_list)
-  (Irmin_git.Branch(Irmin.Branch.String))
-
-module Store_graphql = Irmin_graphql.Make(Store)
-
-let config =
+let config path =
   let head = Git.Reference.of_string "refs/heads/master" in
-  Irmin_git.config ~head (Sys.argv.(1))
+  Irmin_git.config ~head path
 
-let () =
+let run port root contents store =
+  let c = Irmin_unix.Cli.mk_contents contents in
+  let (module Store) = Irmin_unix.Cli.mk_store store c in
+  let module Store_graphql = Irmin_graphql.Make(Store) in
   let p =
-    Store.Repo.v config >>= fun repo ->
+    Store.Repo.v (config root) >>= fun repo ->
     Store.master repo >>= fun store ->
-    Store_graphql.start_server store
+    Store_graphql.start_server ?port store
   in Lwt_main.run p
+
+let port =
+  let doc = "Port to listen on" in
+  Arg.(value & pos 0 (some int) None & info [] ~docv:"PORT" ~doc)
+
+let contents =
+  let doc = "Content type" in
+  Arg.(value & opt string "string" & info ["c"; "contents"] ~docv:"CONTENTS" ~doc)
+
+let store =
+  let doc = "Store type" in
+  Arg.(value & opt string "git" & info ["s"; "store"] ~docv:"STORE" ~doc)
+
+let root =
+  let doc = "Store location" in
+  Arg.(value & opt string "/tmp/irmin" & info ["root"] ~docv:"PATH" ~doc)
+
+let main_t = Term.(const run $ port $ root $ contents $ store)
+let () = Term.exit @@ Term.eval (main_t, Term.info "irmin-graphql")
 
